da05e1ea374e5b55be099c07f8c1114acb3b5613
pedrobuzolin <buzolinpedro@gmail.com>
2026-09-18 12:32:33 -0300

    Implementando Usuario

---
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioService.java
@@ -0,0 +1,21 @@
+package br.com.forum_hub.domain.usuario;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService implements UserDetailsService {
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmailIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado!"));
+    }
+}
